57b16c38089aa5f1fff859ba369e3ba502f94b0e
Marissa Ross <mlyall.ross@ufl.edu>
2020-07-27 21:20:23 -0700

    Add on load and click thru javascript settings

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+    /* HIDE ANIMATION AND NUMBERS ON PAGE LOAD */
+        $('#numbers').hide();
+        $('#glow').hide();
+
+    /* HIDE/SHOW ELEMENTS | FORTUNE OPTIONS CODE */   
+        /* click on purple animation */
+        $('#purple').click(function() {
+            $('#colors').hide();
+            $('#numbers').show();
+
+        });
+    
+        /* click on white animation */
+        $('#white').click(function() {
+            $('#colors').hide();
+            $('#numbers').show();
+
+        })
+
+        /* click on black animation */
+        $('#black').click(function() {
+            $('#colors').hide();
+            $('#numbers').show();
+        
+        })
+
+        /* click on blue animation */
+        $('#blue').click(function() {
+            $('#colors').hide();
+            $('#numbers').show();
+        
+        })
+
+        /* click on one animation */
+        $('#one').click(function() {
+            $('#numbers').hide();
+            $('#poses').show();
+        })
+
+        /* click on two animation */
+        $('#two').click(function() {
+            $('#numbers').hide();
+            $('#poses').show();
+        })
+
+        /* click on three animation */
+        $('#three').click(function() {
+            $('#numbers').hide();
+            $('#poses').show();
+        })
+
+        /* click on four animation */
+        $('#four').click(function() {
+            $('#numbers').hide();
+            $('#poses').show();
+        })
+
+ });
+
